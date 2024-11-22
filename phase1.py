1b4819233da4ccd862d8ed4a6b15318aee91fd87
Elisa Chien <echien@bordeaux-inp.fr>
2024-11-22 11:48:50 +0100

    Ajout d'une fonction de distance

--- phase1.py
+++ phase1.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+def distance(x1,y1,x2,y2):
+    """ Calcule la distance entre deux points """
+    return np.sqrt((x2-x1)**2+(y2-y1)**2)
+
 def echantillonnage(x1,y1,x2,y2,Nb_points):
     X=np.around(np.linspace(np.floor(x1),np.ceil(x2),Nb_points)).astype(int)
     Y=np.around(np.linspace(np.floor(y1),np.ceil(y2),Nb_points)).astype(int)
@@ -7,7 +11,7 @@ def echantillonnage(x1,y1,x2,y2,Nb_points):
 
 def find_lim(x1,y1,x2,y2,img):
     seuil=0
-    X,Y=echantillonnage(x1,y1,x2,y2,np.ceil(np.sqrt((x2-x1)**2+(y2-y1)**2)).astype(int))
+    X,Y=echantillonnage(x1,y1,x2,y2,np.ceil(distance(x1, y1, x2, y2)).astype(int))
     L=img[X,Y]>=seuil
     i1=0
     i2=0
@@ -22,7 +26,7 @@ def find_lim(x1,y1,x2,y2,img):
     return X[i1],Y[i1],X[i2],Y[i2]
 
 def find_u(xd,yd,xa,ya,img,seuil):
-    nb_p=np.ceil(np.sqrt((xa-xd)**2-(ya-yd)**2))
+    nb_p=np.ceil(distance(xd,yd,xa,ya))
     Nb_points=0
     while (Nb_points<nb_p):
         Nb_points+=95
@@ -33,6 +37,6 @@ x1=2
 x2=5
 y1=9
 y2=6
-Nb=np.ceil(np.sqrt((x2-x1)**2+(y2-y1)**2)).astype(int)
+Nb=np.ceil(distance(x1, y1, x2, y2)).astype(int)
 X,Y=echantillonnage(x1,y1,x2,y2,Nb)
 print(X,Y)
