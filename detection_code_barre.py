8ef3ff598d156b9db307da698c358ab4fcd3ca41
David YAN <dyan@enseirb-matmeca.fr>
2024-11-22 10:11:13 +0100

    Implementation de Txx Tyy

--- detection_code_barre.py
+++ detection_code_barre.py
@@ -22,8 +22,21 @@ X, Y = np.meshgrid(y, x)
 
 I_x,I_y=np.gradient(Y_code_barre,x,y)
 #filtre
-def G(x,y,sigma): #derive d'une gaussienne
+def G_2D(n,sigma):
+    x=range(-n,n)
+    y=range(-n,n)
+    X,Y=np.meshgrid(x,y)
+    return np.exp(-1/2*(X**2/(sigma**2)+(Y**2/sigma**2)))
+
+def G_prime(x,y,sigma): #derive d'une gaussienne
     return (-x/(2*np.pi*sigma**2)*np.exp(-(x**2+y**2)/(2*sigma**2)))
+
+gauss2D=G_2D(2,1)
+
+Txx=signal.convolve2d(I_x*I_x,gauss2D,mode='same', boundary='fill', fillvalue=0)
+Tyy=signal.convolve2d(I_y*I_y,gauss2D,mode='same', boundary='fill', fillvalue=0)
+Txy=signal.convolve2d(I_x*I_y,gauss2D,mode='same', boundary='fill', fillvalue=0)
+
 
 plt.figure(1)
 plt.subplot(1, 2, 1)
@@ -41,6 +54,16 @@ plt.subplot(1, 2, 2)
 plt.imshow(I_y,cmap='gray')
 plt.title("grad y")
 
+plt.figure(3)
+plt.subplot(1, 3, 1)
+plt.imshow(Txx,cmap='gray')
+plt.title("Txx")
+plt.subplot(1, 3, 2)
+plt.imshow(Tyy,cmap='gray')
+plt.title("Tyy")
+plt.subplot(1, 3, 3)
+plt.imshow(Txy,cmap='gray')
+plt.title("Txy")
 
 
 plt.show()
