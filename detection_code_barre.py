4045e9404e54590caf11354728e7d9ee540db6f6
never-available <ttall@bordeaux-inp.fr>
2024-11-22 11:49:28 +0100

    ajouts mineurs et rangement du code

--- detection_code_barre.py
+++ detection_code_barre.py
@@ -8,7 +8,8 @@ from skimage import io, color, feature
 from copy import deepcopy
 from scipy import signal, ndimage
 from scipy.interpolate import RectBivariateSpline
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+#%%
 read = lambda title:plt.imread("img/"+title)
 to_ycbcr= lambda img:(color.rgb2ycbcr(img)/255).astype("double")
 to_rgb=lambda img:color.ycbcr2rgb(img*255)
@@ -30,9 +31,15 @@ def plot_channels(channels,titles=None,res_factor=1):
     plt.tight_layout()
     plt.show()
     return 
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PARAMETRES FILTRES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+#%%
+sigma_g=1
+sigma_t=10
+filter_size=100
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+#%%
 # load image
-img_code_barre=plt.imread('img/barcode0.jpg')
+img_code_barre=plt.imread('img/code_barre_prof.jpg')
 
 #transformation de rgb en ycrcb
 img_code_barre_YCbCr=color.rgb2ycbcr(img_code_barre)
@@ -56,11 +63,13 @@ def G_y_prime(n,sigma): #derive d'une gaussienne
     X, Y = np.meshgrid(P,P)
     return (-Y/(2*np.pi*sigma**2)*np.exp(-(X**2+Y**2)/(2*sigma**2)))
 
-sigma_g=10
+
+
 gauss2D=G_2D(2,sigma_g)
-sigma_t=20
-gauss_x_prime=G_x_prime(10,sigma_t)
-gauss_y_prime=G_y_prime(10,sigma_t)
+
+
+gauss_x_prime=G_x_prime(filter_size,sigma_t)
+gauss_y_prime=G_y_prime(filter_size,sigma_t)
 #gradient
 h,w,c=np.shape(img_code_barre_YCbCr)
 x = np.linspace(0, h, h)
@@ -78,13 +87,14 @@ In_y=I_y/norm_I_y
 
 #tenseur de structure local
 
-Txx=signal.convolve2d(I_x*I_x,gauss2D,mode='same', boundary='fill', fillvalue=0)
-Tyy=signal.convolve2d(I_y*I_y,gauss2D,mode='same', boundary='fill', fillvalue=0)
-Txy=signal.convolve2d(I_x*I_y,gauss2D,mode='same', boundary='fill', fillvalue=0)
+Txx=signal.convolve2d(In_x*In_x,gauss2D,mode='same', boundary='fill', fillvalue=0)
+Tyy=signal.convolve2d(In_y*In_y,gauss2D,mode='same', boundary='fill', fillvalue=0)
+Txy=signal.convolve2d(In_x*In_y,gauss2D,mode='same', boundary='fill', fillvalue=0)
 
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mesure de coherence ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mesure de cohérence ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+#%%
 # méthode avec les valeurs propres possibles seulement si la matrice est crrée
 T=np.block([[Txx,Txy],[Txy,Tyy]])
 # dt=np.linalg.det(T)
@@ -92,35 +102,10 @@ T=np.block([[Txx,Txy],[Txy,Tyy]])
 
 D = lambda X,Y,Z: np.sqrt((X-Y)**2+4*Z**2)/(X+Y)
 D_res=D(Txx,Tyy,Txy)
-
-plot_channels([img_code_barre,D_res])
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mesure de coherence ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-# plt.figure(1)
-# plt.subplot(1, 2, 1)
-# plt.imshow(img_code_barre)
-# plt.title("img origine")
-# plt.subplot(1, 2, 2)
-# plt.imshow(img_code_barre_YCbCr[:,:,0],cmap='gray')
-# plt.title("img canal Y")
-
-# plt.figure(2)
-# plt.subplot(1, 2, 1)
-# plt.imshow(I_x,cmap='gray')
-# plt.title("grad x")
-# plt.subplot(1, 2, 2)
-# plt.imshow(I_y,cmap='gray')
-# plt.title("grad y")
-
-# plt.figure(3)
-# plt.subplot(1, 3, 1)
-# plt.imshow(Txx,cmap='gray')
-# plt.title("Txx")
-# plt.subplot(1, 3, 2)
-# plt.imshow(Tyy,cmap='gray')
-# plt.title("Tyy")
-# plt.subplot(1, 3, 3)
-# plt.imshow(Txy,cmap='gray')
-# plt.title("Txy")
+D_seuil=D>0.8
 
 
-# plt.show()
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Affichage ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+#%%
+plot_channels([img_code_barre,D_res,D_seuil],["image originale","mesure de cohérence","image seuilée"],res_factor=2)
+
