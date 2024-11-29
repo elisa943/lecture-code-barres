839a4bc9596152a2f45abf3bebd8d81da99d0d8a
Alban Oberti <alban.oberti@bordeaux-inp.fr>
2024-11-29 13:55:08 +0100

    Si vous lisez ce message vous avez vait un git log

--- phase1.py
+++ phase1.py
@@ -46,6 +46,38 @@ def separate(l_bin,u):
         L[i,:]=l_bin[start+i*7*u:start+(i+1)*7*u]
     return L
 
+def compare(L_exp,L_the):
+    min=len(L_exp[0])
+    u=len(L_exp[0])/7
+    decode=np.zeros(1,12)
+    r=np.zeros(1,6)
+    L_compar=np.copy(L_the)
+    for j in range(0,len(L_the[0])):
+        L_compar[1,j]=("{0:b}".format(L_the[1,i]).zfill(7))*u
+        L_compar[2,j]=("{0:b}".format(L_the[2,i]).zfill(7))*u
+        L_compar[3,i]=("{0:b}".format(L_the[3,i]).zfill(7))*u
+    for i in range(0,len(L_exp)/2):
+        for j in range(0,len(L_compar[0])):
+            if (min>sum(L_exp[i]!=L_compar[1,j])):
+                min=sum(L_exp[i]!=L_compar[1,j])
+                decode[i]=L_the[1,j]
+                r[i]=1
+            if (min>sum(L_exp[i]!=L_compar[2,j])):
+                min=sum(L_exp[i]!=L_compar[2,j])
+                decode[i]=L_the[2,j]
+                r[i]=2
+        min=len(L_exp[0])
+    for i in range(0,len(L_exp)/2):
+        for j in range(0,len(L_compar[0])):
+            if (min>sum(L_exp[i]!=L_compar[3,j])):
+                min=sum(L_exp[i]!=L_compar[1,j])
+                decode[i]=L_the[1,j]
+        min=len(L_exp[0])
+    return decode,r
+    
+    
+        
+
 x1=2
 x2=5
 y1=9
@@ -53,3 +85,4 @@ y2=6
 Nb=np.ceil(distance(x1, y1, x2, y2)).astype(int)
 X,Y=echantillonnage(x1,y1,x2,y2,Nb)
 print(X,Y)
+L=[[13,25,19,61,35,49,47,59,55,11],[43,51,27,33,29,57,5,17,9,23],[114,102,108,66,92,78,80,68,72,116]]
