f91783b3ee3921e74fd33abffbd55f5b867147b5
never-available <ttall@bordeaux-inp.fr>
2024-11-29 17:22:28 +0100

    amélioration de la vitesse d'exécution

--- detection_code_barre.py
+++ detection_code_barre.py
@@ -10,6 +10,7 @@ from scipy import signal, ndimage
 from scipy.interpolate import RectBivariateSpline
 from skimage.morphology import closing, square
 from time import time
+from scipy.signal import fftconvolve
 start_time=time()
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # %%
@@ -34,22 +35,20 @@ def plot_channels(channels, titles=None, res_factor=1):
         plt.imshow(channels[i], cmap='gray')
         plt.title(titles[i])
 
-    plt.tight_layout()
-    plt.show()
+    # plt.tight_layout()
     return
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PARAMETRES FILTRES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # %%
 # Pour le bruit
-sigma_bruit=1.5
+sigma_bruit=2.1
 
 # Pour le gradient
 sigma_g = 1     
 
 # Pour le tenseur
 sigma_t = 15
-
 
 """
 sigma canny:
@@ -63,7 +62,7 @@ sigma T:
 
 img_code_barre = plt.imread('img/code_barre_prof.jpg')
 
-
+print("test")
 # ~~~~~~~~~~~~~~~~~~~~ transformation de rgb en ycrcb  ~~~~~~~~~~~~~~~~~~~
 img_code_barre_YCbCr = color.rgb2ycbcr(img_code_barre)
 Y_code_barre = img_code_barre_YCbCr[:, :, 0]
@@ -71,7 +70,7 @@ Cb_code_barre = img_code_barre_YCbCr[:, :, 1]
 Cr_code_barre = img_code_barre_YCbCr[:, :, 2]
 Y_code_barre+= np.random.randn(len(Y_code_barre),len(Y_code_barre[0]))*sigma_bruit
 # ~~~~~~~~~~~~~~~~~~~ filtre ~~~~~~~~~~~~~~~~~~~
-
+print("test")
 
 def G_2D(sigma):
     x = range(floor(-3*sigma), floor(3*sigma+1))
@@ -92,9 +91,7 @@ def G_y_prime(sigma):  # derive d'une gaussienne
     return (-Y/(2*np.pi*sigma**4)*np.exp(-(X**2+Y**2)/(2*sigma**2)))
 
 
-
-
-
+print("test")
 gauss_x_prime = G_x_prime(sigma_g)
 gauss_y_prime = G_y_prime(sigma_g)
 # ~~~~~~~~~~~~~~~~~~~ gradient ~~~~~~~~~~~~~~~~~~~
@@ -102,59 +99,88 @@ h, w, c = np.shape(img_code_barre_YCbCr)
 x = np.linspace(0, h, h)
 y = np.linspace(0, w, w)
 X, Y = np.meshgrid(y, x)
-
-I_x = signal.convolve2d(Y_code_barre, gauss_x_prime,
-                        mode='same', boundary='fill', fillvalue=0)
-I_y = signal.convolve2d(Y_code_barre, gauss_y_prime,
-                        mode='same', boundary='fill', fillvalue=0)
+print("test")
+# I_x = signal.convolve2d(Y_code_barre, gauss_x_prime,1mode='same', boundary='fill', fillvalue=0)
+I_x = fftconvolve(Y_code_barre, gauss_x_prime, mode='same')
+print("test")
+I_y = fftconvolve(Y_code_barre, gauss_y_prime, mode='same')
 # ~~~~~~~~~~~~~~~~~~~ Normalisation ~~~~~~~~~~~~~~~~~~~
-default_value = (0, 0)
-delta_I=  [[default_value for _ in range(w)] for _ in range(h)]
+# default_value = (0, 0)
+# delta_I=  [[default_value for _ in range(w)] for _ in range(h)]
+print("test")
+# for i in range(h):
+#     for j in range(w):
+#         delta_I[i][j]=(I_x[i][j],I_y[i][j])
 
-for i in range(h):
-    for j in range(w):
-        delta_I[i][j]=(I_x[i][j],I_y[i][j])
+delta_I = np.stack((I_x, I_y), axis=-1)
 
-N_delta_I = [[np.sqrt(x**2 + y**2) for x, y in ligne] for ligne in delta_I]
+N_delta_I = np.linalg.norm(delta_I, ord=2, axis=-1)
+# N_delta_I = [[np.sqrt(x**2 + y**2) for x, y in ligne] for ligne in delta_I]
 
+print("test")
 N_I_x=np.divide(I_x,N_delta_I)
 N_I_y=np.divide(I_y,N_delta_I)
 # ~~~~~~~~~~~~~~~~~~~ tenseur de structure local ~~~~~~~~~~~~~~~~~~~
+print("test")
 gauss2D = G_2D(sigma_t)
-Txx = signal.convolve2d(N_I_x*N_I_x, gauss2D, mode='same',
-                        boundary='fill', fillvalue=0)
-Tyy = signal.convolve2d(N_I_y*N_I_y, gauss2D, mode='same',
-                        boundary='fill', fillvalue=0)
-Txy = signal.convolve2d(N_I_x*N_I_y, gauss2D, mode='same',
-                        boundary='fill', fillvalue=0)
+print("test")
 
+Txx = fftconvolve(N_I_x * N_I_x, gauss2D, mode='same')
+Tyy = fftconvolve(N_I_y * N_I_y, gauss2D, mode='same')
+Txy = fftconvolve(N_I_x * N_I_y, gauss2D, mode='same')  
 
+print("test")
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mesure de cohérence ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # %%
-# méthode avec les valeurs propres possibles seulement si la matrice est crrée
+# méthode avec les valeurs propres possibles seulement si la matrice est crée
 T = np.block([[Txx, Txy], [Txy, Tyy]])
 # dt=np.linalg.det(T)
 # U, S, Vt = np.linalg.svd(T)
 D = lambda X,Y,Z: np.sqrt((X-Y)**2+4*Z**2)/(X+Y)
+print("test")
 D_res=D(Txx,Tyy,Txy)
-
+print("test")
 D_seuil=(D_res>0.7)
-
+print("test")
 #plot_channels([img_code_barre,D_seuil])
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mesure de coherence ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-
-# D=lambda X, Y, Z: np.sqrt((X-Y)**2+4*Z**2)/(X+Y)
-
-
-# D_res = D(Txx, Tyy, Txy)
-# D_seuil = D_res < 0.8
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Closing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 
 D_seuil_closed = closing(D_seuil, square(3))
-
+print("test")
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Affichage ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # plot_channels([img_code_barre, D_res, D_seuil, D_seuil_closed], ["image originale",
 #               "mesure de cohérence", "image seuilée", "Image après Fermeture"], res_factor=2)
+
+# # Affichage 1: Image originale et canal Y
+# plot_channels([img_code_barre, img_code_barre_YCbCr[:, :, 0]], 
+#               titles=["img origine", "img canal Y"], 
+#               res_factor=2)
+
+# # Affichage 2: Gradients x et y
+# plot_channels([I_x, I_y], 
+#               titles=["grad x", "grad y"], 
+#               res_factor=2)
+
+# # Affichage 3: Gradients normalisés x et y
+# plot_channels([N_I_x, N_I_y], 
+#               titles=["normalisé grad x", "normalisé grad y"], 
+#               res_factor=2)
+
+# # Affichage 4: Tenseurs Txx, Tyy et Txy
+# plot_channels([Txx, Tyy, Txy], 
+#               titles=["Txx", "Tyy", "Txy"], 
+#               res_factor=2)
+
+# # Affichage 5: Mesure D et D seuil
+# plot_channels([D_res, D_seuil], 
+#               titles=["D", "D seuil"], 
+#               res_factor=2)
+
+# plt.show()
+
+
+print("test")
 
 plt.figure(1)
 plt.subplot(1, 2, 1)
@@ -200,8 +226,8 @@ plt.imshow(D_seuil,cmap='gray')
 plt.title("D seuil")
 
 
-plt.show()
-print("f{time()-start_time} s")
+# plt.show()
+print(f"{time()-start_time} s")
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Lancer aléatoire ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # %%
 def bornage(h,w,p):
