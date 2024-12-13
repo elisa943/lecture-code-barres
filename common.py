0a50310ff364f51edcd104a1e9bfbf5a7d41739e
never-available <ttall@bordeaux-inp.fr>
2024-12-13 14:53:15 +0100

    fonctions de tracé de rayons dans common.py

--- common.py
+++ common.py
@@ -21,3 +21,45 @@ def bornage2(h,w,ray):
     # unused for now
     ray=np.array(ray)
     return [bornage(h,w,ray[0]),bornage(h,w,ray[1])]
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Fonctions tracé de rayons ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# %%
+
+
+# ajouter des paramètres pour avoir un tirage gaussien (ou autre)
+
+def ray_center(center,length,angle):
+    """
+    Parameters
+    ----------
+    center : np.array([x0,y0])
+    length : float
+    angle : float
+    crée un rayon de centre center, de longueur length et d'angle alpha (par rapport à l'horizontale)
+    """
+    r = length/2
+    offset = np.array([np.cos(angle), np.sin(angle)])*r
+    x1 = center+offset
+    x2 = center-offset
+    return np.array([x1,x2])
+
+def random_ray_center(h, w, length):
+    # méthode: centre, angle, longueur
+    angle = np.random.uniform(0, 2*np.pi)
+    r = length/2
+    center = np.array([np.random.randint(0, h), np.random.randint(0, w)])
+    offset = np.array([np.cos(angle), np.sin(angle)])*r
+    x1 = center+offset
+    x2 = center-offset
+    return np.int32([bornage(h, w, x1), bornage(h, w, x2)])
+
+
+def random_ray(h, w, length):
+    # méthode: extrémité1, angle, longueur
+    angle = np.random.uniform(0, 2*np.pi)
+
+    x1 = np.array([np.random.randint(0, h), np.random.randint(0, w)])
+    offset = np.array([np.cos(angle), np.sin(angle)])*length
+    x2 = x1+offset
+
+    return np.int32([bornage(h, w, x1), bornage(h, w, x2)])
